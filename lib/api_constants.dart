d93df82c8ff98427f10172170dad015b3d912891
Junaid Ahmed <j.ahmed.ja7a7@gmail.com>
2024-12-21 01:19:30 +0500

    Dummy Values

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -6,6 +6,6 @@
  2. Replace the placeholder values with your actual API keys. */
 class ApiConstants {
   static const String azureTranslateApiKey =
-      "1UMHYGpC45IIwuwIsZfZhjZ63CSFXWIhcDN9xxloiWWfIIwRzCCXJQQJ99ALACULyCpXJ3w3AAAbACOGvOAo";
-  static const String azureRegion = "global";
+      "example_key";
+  static const String azureRegion = "region_name";
 }
